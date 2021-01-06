e8dc977e35fcbe8cb5b4edee2629b4322cd10fcf
Nanne <n.j.e.vannoord@uva.nl>
2021-01-06 17:38:05 +0100

    var placement

--- dane_server/RabbitMQListener.py
+++ dane_server/RabbitMQListener.py
@@ -30,8 +30,8 @@ class RabbitMQListener(RabbitMQHandler):
     internal_lock = threading.RLock()
 
     def __init__(self, config):
-        super().__init__(config)
         self._connected = False
+        super().__init__(config)
 
     def connect(self):
         if not self._connected:
